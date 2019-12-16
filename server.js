e2fde1076e1894251ba978476d388eea11793dc7
arussell36 <andrewrussell036@gmail.com>
2019-12-15 19:35:15 -0600

    inqurier functioning and initial questions created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // GLOBAL VARIABLES AND DEPENDENCIES //
 const mysql = require("mysql");
+const inquirer = require('inquirer');
 
 
 // CONNECTION //
@@ -11,7 +12,51 @@ const connection = mysql.createConnection({
   database: "jobs_db"
 });
 
-connection.connect(function(err) {
+connection.connect((err) => {
   if (err) throw err;
   console.log(`Your are now connected with ID: ${connection.threadId}`);
+  runSearch();
 });
+
+// OPENING SEQUENCE OF QUESTIONS //
+function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "Welcome to the THEJOB! What would you like to do?",
+        choices: [
+          "View all Departments",
+          "View all Roles",
+          "View all Employees",
+          "Add an Employee",
+          "Update Employee Role",
+          "exit"
+        ]
+
+      }).then(function(answer) {
+        console.log(answer.action);
+        if (answer.action === 'View all Departments') {
+            // console.log('view dept')
+            viewDept();
+        } else if (answer.action === 'View all Roles') {
+            // console.log('view role')
+            viewRole();
+        } else if (answer.action === 'View all Employees') {
+            // console.log('view empl')
+            viewEmpl();
+        } else if (answer.action === 'Add an Employee') {
+            // console.log('add empl')
+            addEmpl();
+        } else if (answer.action === 'Update Employee Role') {
+            // console.log('update empl')
+            updateEmpl();
+        } else {
+            // console.log('terminate')
+            connection.end();
+        };
+  });
+
+};
+
+// EXECTUABLE FUNCTIONS THAT CONNECT TO MYSQL // 
